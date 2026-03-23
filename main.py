8859b57291869e935dec12532ecf74f9d3a4b350
mayanksharma9225 <mayanksharma9225@gmail.com>
2026-03-23 14:36:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import cv2
 import pytesseract
 
+# If Tesseract not detected, uncomment below line and set path
+# pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
 def extract_text(image_path):
     image = cv2.imread(image_path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
